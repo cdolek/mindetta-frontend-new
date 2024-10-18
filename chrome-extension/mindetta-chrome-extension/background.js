d9b045a83bccd58ed5d4c69bbba441c28c564cf6
Cenk <cdolek@gmail.com>
2024-10-18 12:56:10 -0400

    mindetta chrome extension

---
+++ chrome-extension/mindetta-chrome-extension/background.js
@@ -0,0 +1,55 @@
+chrome.action.onClicked.addListener((tab) => {
+  let notificationId = `mindetta-video-${Date.now()}`;
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const url = new URL(tab.url);
+    const videoId = url.searchParams.get("v");
+    if (videoId) {
+      fetch("http://178.128.150.234:8000/api/v1/insert", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ videoId }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("API Triggered Successfully");
+            chrome.notifications.create(notificationId, {
+              type: "basic",
+              iconUrl: "open-mind.png",
+              title: "Video Queued",
+              message: `Video ID: ${videoId} is in the queue!`,
+            });
+          } else {
+            console.error("Failed to trigger API");
+            throw new Error("Failed to trigger API");
+          }
+        })
+        .catch((error) => {
+          console.error("Error: " + error);
+          chrome.notifications.create(notificationId, {
+            type: "basic",
+            iconUrl: "open-mind.png",
+            title: "Error",
+            message: "Failed to queue the video. Please try again.",
+          });
+        });
+    } else {
+      console.error("Unable to determine video ID");
+      chrome.notifications.create(notificationId, {
+        type: "basic",
+        iconUrl: "open-mind.png",
+        title: "Error",
+        message: "Unable to determine video ID.",
+      });
+    }
+  } else {
+    console.error("This is not a YouTube video page");
+    chrome.notifications.create(notificationId, {
+      type: "basic",
+      iconUrl: "open-mind.png",
+      title: "Error",
+      message: "This is not a YouTube video page.",
+    });
+  }
+});
