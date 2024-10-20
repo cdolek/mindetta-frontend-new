ca098c86def989fbdd91a24db595b2a62c55ac67
Cenk <cdolek@gmail.com>
2024-10-20 19:01:40 -0400

    Create GraphVis.tsx

---
+++ src/app/_components/GraphVis.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import Graph from "graphology";
+import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
+import "@react-sigma/core/lib/react-sigma.min.css";
+
+const sigmaStyle = { height: "500px", width: "500px" };
+
+// Component that load the graph
+export const LoadGraph = () => {
+  const loadGraph = useLoadGraph();
+
+  useEffect(() => {
+    const graph = new Graph();
+    graph.addNode("first", {
+      x: 0,
+      y: 0,
+      size: 15,
+      label: "My first node",
+      color: "#FA4F40",
+    });
+    loadGraph(graph);
+  }, [loadGraph]);
+
+  return null;
+};
+
+// Component that display the graph
+export const DisplayGraph = () => {
+  return (
+    <SigmaContainer style={sigmaStyle}>
+      <LoadGraph />
+    </SigmaContainer>
+  );
+};
